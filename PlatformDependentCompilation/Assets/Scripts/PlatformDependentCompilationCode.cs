bba034516cd1b7ade4b479b13352c87b536bd775
Sebastien Bovo <sebastienbovo@outlook.com>
2018-01-26 16:23:10 +0100

    Very first Platform Dependent code

---
+++ PlatformDependentCompilation/Assets/Scripts/PlatformDependentCompilationCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDependentCompilationCode : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+#if NETFX_CORE
+        Debug.Log("Execution in UWP");
+#endif
+#if UNITY_EDITOR
+        Debug.Log("Execution in Unity Editor");
+#endif
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
